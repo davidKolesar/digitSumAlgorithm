d0cf8c671b4c7e24ccaa52246ef3fd7094aa5ec0
whiteWasp <djkolesa@gmail.com>
2019-06-08 17:34:57 -0400

    David Kolesar is commiting initial work

---
+++ digitSumSolution.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.Test;
+public class RemovedNumbers {
+	
+	public static List<long[]> removNb(long n) {
+		
+    //break down each digit
+    int individualDigit;
+    
+    while (n > 0) {
+        System.out.println( n % 10);
+        n = n / 10;
+    }
+    
+    
+		return null;
+	}
+}
