a21eb3e1d9bef2b2a6ad4bee50b85ca473b7c0ee
whiteWasp <djkolesa@gmail.com>
2019-06-08 17:44:49 -0400

    David Kolesar is commiting initial work

--- digitSumSolution.java
+++ digitSumSolution.java
@@ -5,17 +5,28 @@ public class RemovedNumbers {
 	
 	public static List<long[]> removNb(long n) {
 		
-    //break down each digit
+    //break down n into each individual digit
     long individualDigit;
+    long sumOfAllDigits;
     
     while (n > 0) {
         System.out.println("The first number is : " + n);
+        // n % 10 will give us the last digit in the series of numbers
         individualDigit = (n % 10);
         System.out.println("The first digit of that number is : " + n % 10);
+        // reassign n to it's new value (so we don't keep getting the same digit forever)
         n = n / 10;
+        
+        //add all digits to get sum of digits
+        //muyltiply each pair of digits
+        //compare to sum
     }
-    
     
 		return null;
 	}
+  
+  	public static void calculateDigits (long n) { 
+    
+    }
+  
 }
