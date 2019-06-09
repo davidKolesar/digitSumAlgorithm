5b7f206b5cc4e21ac6c466dddcc8bb9b0382fcc2
whiteWasp <djkolesa@gmail.com>
2019-06-09 09:19:21 -0400

    David is redesigning solution

--- digitSumSolution.java
+++ digitSumSolution.java
@@ -7,13 +7,13 @@ public class RemovedNumbers {
 	public static List<long[]> removNb(long n) {
     List<Long> allNumbersInN = new ArrayList<Long>();
 	
-  //get each number between 1 and n
+      //get each number between 1 and n
       for(long i = 0; i < n; i++) {
           allNumbersInN.add(i);
       }
     
     //select two digits (exclusive of themselves)
-    for(long digit : allNumbersInN) {
+    for(long number : allNumbersInN) {
       int index = 0;
       
       for(int i = 0; i < allNumbersInN.size(); i++) {
@@ -21,17 +21,30 @@ public class RemovedNumbers {
             i++;
         }
         long numberToCompare = allNumbersInN.indexOf(i);
-  
+        long productOfTwoNumbers = (numberToCompare * number); 
+
+        //add up all numbers (minus the two in question)
+        long sumOfAllNumbers;
       }
     
     
     }
     
+  
     
-    //add up all digits (minus the two in question)
+    
     
     //multiply two comparison digits
     
       return null;
   }
+  
+  private static addUpAllNumbersInList(List<Long> allNumbersInN ) {
+    long sumOfAllNumbers = 0;
+    for(long number : allNumbersInN) {
+    
+      sumOfAllNumbers = (sumOfAllNumbers + number);
+    }
+  
+    return sumOfAllNumbers;
 }
