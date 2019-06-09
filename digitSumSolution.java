6a4beb0f0c83993596f743e9ffcdd4c924e59f02
whiteWasp <djkolesa@gmail.com>
2019-06-08 21:18:58 -0400

    David Kolesar is working on code problem

--- digitSumSolution.java
+++ digitSumSolution.java
@@ -1,12 +1,13 @@
 import java.util.List;
 import java.util.ArrayList;
 import org.junit.Test;
+import static java.lang.Math.toIntExact;
 public class RemovedNumbers {
 	
 	public static List<long[]> removNb(long n) {
 		
     //break down n into each individual digit
-    long individualDigit;
+    long currentDigit;
     long sumOfAllDigits = 0;
     List<Long> digitsToAdd = new ArrayList<Long>();
     List<Long> products = new ArrayList<Long>();
@@ -14,8 +15,27 @@ public class RemovedNumbers {
     while (n > 0) {
         // n % 10 will give us the last digit in the series of numbers
         System.out.println("The first number is : " + n);
-        individualDigit = (n % 10);
+        currentDigit = (n % 10);
 
+        /*
+        *to save memory and make work easeir, we can convert each long to 
+        *an integer since each one is only one digit (and will therefore
+        *convert correctly). With Java 8 'java.lang.Math.toIntExact', it
+        * will throw an arithmetic exception in case of overflow. See
+        *https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#toIntExact-long-
+        */
+        try {
+          int individualDigit = toIntExact(currentDigit);
+        } 
+          catch(Exception e) {
+            /*
+            * TODO: This will terminate the program in case of overflow. There could be
+            * another code path that handles the exception by executing a flow with Longs.
+            */
+            System.out.println("Long overflowed during conversion to int. This is a terminal failure. Error : " + e);
+            System.exit(0); 
+          }
+                  
         //store each individual digit
         digitsToAdd.add(individualDigit);
 
@@ -28,6 +48,7 @@ public class RemovedNumbers {
     sumOfAllDigits = AddAllDigits(digitsToAdd);
         
     //multiply each pair of digits
+    generateCombinations(digitsToAdd);
     
     //compare to sum
     
@@ -49,7 +70,7 @@ public class RemovedNumbers {
  *Uses Iterative Algorithm to generates combinations by combining
  * all possible *pairs  of multiples (does not work for not permutations)
  */
-private static void generateCombinations() {
+private static void generateCombinations(List<Long> digitsToAdd) {
 
 int[] input = {10, 20, 30, 40, 50};    // input array
 
